31a0785a4337d12a11bea695d5f648e5813eb86b
tejaswiniii-11 <tejaswinitodkar11@gmail.com>
2025-10-18 02:33:26 +0530

    Made changes in dashboard and home page of employee interface

--- src/app/employee/components/employee-home/home-component/home-component.ts
+++ src/app/employee/components/employee-home/home-component/home-component.ts
@@ -10,4 +10,19 @@ export class HomeComponent {
 
   organizationName: string = "AurionPro Solutions";
 
+  currentDate: Date = new Date();
+  currentTime: string = '';
+
+  ngOnInit(): void {
+    this.updateDateTime();
+  }
+
+  updateDateTime(): void {
+    // Update time every second
+    setInterval(() => {
+      this.currentDate = new Date();
+      this.currentTime = this.currentDate.toLocaleTimeString();
+    }, 1000);
+  }
+
 }
